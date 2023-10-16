11ce37fb4e047f7fb3b046a572b2879f98b3f61a
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-10-16 17:15:43 +0300

    4th task completed

--- src/js/ToDoList-lab.js
+++ src/js/ToDoList-lab.js
@@ -1,22 +1,42 @@
 const tasks = document.getElementsByTagName("li");
 
-for (let i = 0; i < tasks.length; i++) {
+function createButton() {
     let buttonElement = document.createElement("button");
     let textNode = document.createTextNode("Delete");
     buttonElement.appendChild(textNode);
     buttonElement.className = "close";
     buttonElement.addEventListener("click", function () {
-        // this.parentElement.style.display = "none";
-        tasks[i].style.display = "none";
+        this.parentElement.style.display = "none";
     })
+    return buttonElement;
+}
+
+for (let i = 0; i < tasks.length; i++) {
+    let buttonElement = createButton();
     tasks[i].appendChild(buttonElement);
 }
 
-var list = document.querySelector("ul");
+const list = document.querySelector("ul");
 
 list.addEventListener("click", function (event) {
     if (event.target.tagName === "LI") {
         event.target.classList.toggle("checked")
     }
 }, false)
+
+function newTask() {
+    let input = document.getElementById("myInput").value.trim();
+    if (!input) {
+        alert("Please enter something useful here!")
+        return;
+    }
+    let buttonElement = createButton();
+    let listItem = document.createElement("li");
+    let text = document.createTextNode(input);
+    listItem.appendChild(text)
+    listItem.appendChild(buttonElement)
+    document.getElementById("myUL").appendChild(listItem);
+
+    document.getElementById("myInput").value = "";
+}
 
