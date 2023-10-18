06f5ce0b0f941b7ffaefbe5eebc9445e5960fd5e
Konstantin Tenman <ktenman@users.noreply.github.com>
2023-10-18 13:09:14 +0300

    Refactor to the last version

--- src/js/ToDoList-lab.js
+++ src/js/ToDoList-lab.js
@@ -1,39 +1,39 @@
-const tasks = document.getElementsByTagName("li");
-
-function createButton() {
-    let buttonElement = document.createElement("button");
-    let textNode = document.createTextNode("Delete");
-    buttonElement.appendChild(textNode);
+function createDeleteButton() {
+    const buttonElement = document.createElement("button");
+    buttonElement.textContent = "Delete";
     buttonElement.className = "close";
     buttonElement.addEventListener("click", () => buttonElement.parentElement.style.display = "none");
     return buttonElement;
 }
 
-for (const task of tasks) {
-    task.appendChild(createButton());
+function addDeleteButtonToTasks() {
+    const tasks = document.querySelectorAll("li");
+    for (const task of tasks) {
+        task.appendChild(createDeleteButton());
+    }
 }
 
-const list = document.querySelector("ul");
-
-list.addEventListener("click", function (event) {
-    if (event.target.tagName === "LI") {
-        event.target.classList.toggle("checked")
-    }
-}, false)
-
-function newTask() {
-    let input = document.getElementById("myInput").value.trim();
-    if (!input) {
-        alert("Please enter something useful here!")
+function toggleTaskCompletion(event) {
+    if (event.target.tagName !== "LI") {
         return;
     }
-    let buttonElement = createButton();
-    let listItem = document.createElement("li");
-    let text = document.createTextNode(input);
-    listItem.appendChild(text)
-    listItem.appendChild(buttonElement)
-    document.getElementById("myUL").appendChild(listItem);
+    event.target.classList.toggle("checked");
+}
 
+function newTask() {
+    const inputText = document.getElementById("myInput").value.trim();
+
+    if (!inputText) {
+        alert("Please enter something useful here!");
+        return;
+    }
+
+    const listItem = document.createElement("li");
+    listItem.textContent = inputText;
+    listItem.appendChild(createDeleteButton());
+    document.getElementById("myUL").appendChild(listItem);
     document.getElementById("myInput").value = "";
 }
 
+addDeleteButtonToTasks();
+document.querySelector("ul").addEventListener("click", toggleTaskCompletion, false);
